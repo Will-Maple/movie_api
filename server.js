09c4364410e931941b7e6f9de0e405514af9ea02
Will-Maple17 <will.maple17@gmail.com>
2024-10-25 12:46:45 -0500

    Create log.txt Update server.js to parse url

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
-const http = require('http');
+const http = require('http'),
+    url = require('url'),
+    fs = require('fs');
 
 http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello Node!\n'); 
+
+    let addr = request.url,
+    q = new URL(addr, 'http://localhost:8080')
+    filePath = '';
+
+    if (q.pathname.includes('documentation')) {
+        filePath = (__dirname + '/documentation.html');
+        } else {
+            filepath = 'index.html';
+    }
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            throw err;
+        }
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end('I will have what she is having!\n'); 
+    });
+
 }).listen(8080);
 
 console.log('My first Node test Server is running on Port 8080');
