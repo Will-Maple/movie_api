123d7cac1f38d12b3f4272ea78fd825b6d6c5bbf
Will-Maple17 <will.maple17@gmail.com>
2024-10-28 10:42:03 -0500

    Update index.js added Logger with Time

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+let myLogger = (req, res, next) => {
+    console.log (req.url);
+    next();
+};
+
+let requestTime = (req, res, next) => {
+    req.requestTime = Date.now();
+    next();
+}
+
+app.use(myLogger);
+app.use(requestTime);
+
 app.get('/movies', (req, res) => {
     res.json(movies);
 });
