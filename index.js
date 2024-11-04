30689d6fcfd609a8fa04d609e17978ed5f32ae92
Will-Maple17 <will.maple17@gmail.com>
2024-11-04 15:27:11 -0600

    Update Index.js global movies, users, required bp and uuid

--- index.js
+++ index.js
@@ -1,59 +1,69 @@
 const express = require('express'),
     morgan = require('morgan'),
     fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    uuid = require('uuid');
 
 const app = express(),
     accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'});
 
 app.use(morgan('combined', {stream: accessLogStream}));
+app.use(bodyParser.json());
+
+let users = [
+
+],
+    movies = [
+{
+    Title: "Shadows of Our Forgotton Anscestors",
+    Year: "1964"
+},
+{
+    Title: "Visitor of a Museum",
+    Year: "1989"
+},
+{
+    Title: "Mothlight",
+    Year: "1963"
+},
+{
+    Title: "News from Home",
+    Year: "1977"
+},
+{
+    Title: "J'accuse!",
+    Year: "1919"
+},
+{
+    Title: "Marketa Lazarova",
+    Year: "1967"
+},
+{
+    Title: "Tokyo Story",
+    Year: "1953"
+},
+{
+    Title: "The Mirror",
+    Year: "1975"
+},
+{
+    Title: "Ordet",
+    Year: "1955"
+},
+{
+    Title: "Pastoral: To Die in the Country",
+    Year: "1974"
+}
+];
 
 app.get('/', (req, res) => {
-    res.send('Boo! Halloween, Boo!!');
+    res.send('public/documentation.html');
 })
 
+//Read
 app.get('/movies', (req, res) => {
-    res.json({Movie1: {
-        Title: "Shadows of Our Forgotton Anscestors",
-        Year: "1964"
-    },
-    Movie2: {
-        Title: "Visitor of a Museum",
-        Year: "1989"
-    },
-    Movie3: {
-        Title: "Mothlight",
-        Year: "1963"
-    },
-    Movie4: {
-        Title: "News from Home",
-        Year: "1977"
-    },
-    Movie5: {
-        Title: "J'accuse!",
-        Year: "1919"
-    },
-    Movie6: {
-        Title: "Marketa Lazarova",
-        Year: "1967"
-    },
-    Movie7: {
-        Title: "Tokyo Story",
-        Year: "1953"
-    },
-    Movie8: {
-        Title: "The Mirror",
-        Year: "1975"
-    },
-    Movie9: {
-        Title: "Ordet",
-        Year: "1955"
-    },
-    Movie10: {
-        Title: "Pastoral: To Die in the Country",
-        Year: "1974"
-    }
-});
+    res.status(200).json(movies);
 });
 
 app.use(express.static('public'));
