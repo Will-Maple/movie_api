550d44d5e53b4acd3253c6438bddd68434b76756
Will-Maple17 <will.maple17@gmail.com>
2024-11-16 19:49:40 -0600

    Update index.js added get all users and get user

--- index.js
+++ index.js
@@ -118,7 +118,30 @@ app.get('/', (req, res) => {
     res.status(200).sendFile(path.join(__dirname, '/public/documentation.yaml'));
 })
 
-// Read all
+// Read All Users
+app.get('/users', async (req, req) => {
+    await Users.find()
+        .then((users) => {
+            res.status(201).json(users);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+
+app.get('/users/:Username', async (req, res) => {
+    await Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+        res.json(user);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
+
+// Read all Movies
 app.get('/movies', (req, res) => {
     res.status(200).json(movies);
 });
